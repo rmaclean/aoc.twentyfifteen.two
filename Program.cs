e25f9e3a1da890736c167773ceebe2c249c3df6a
Robert MacLean <robert@sadev.co.za>
2020-12-10 11:17:14 +0200

    step 2

--- Program.cs
+++ Program.cs
@@ -6,17 +6,37 @@ using System.IO;
 using System.Linq;
 
 var parser = new Regex("(?<l>\\d+)x(?<w>\\d+)x(?<h>\\d+)");
+var boxes = File
+    .ReadAllLines("data.txt")
+    .Select(line => ParseLine(line));
+
 Part1();
+Part2();
+
+void Part2()
+{
+    var ribbonNeeded = boxes
+        .Select(dimensions => RibbonNeeded(dimensions))
+        .Sum();
+
+    Console.WriteLine($"We need {ribbonNeeded} feet of ribbon");
+}
 
 void Part1()
 {
-    var paperNeed = File
-        .ReadAllLines("data.txt")
-        .Select(line => ParseLine(line))
+    var paperNeed = boxes
         .Select(dimensions => PaperNeeded(dimensions))
         .Sum();
 
     Console.WriteLine($"We need {paperNeed} square feet of paper");
+}
+
+int RibbonNeeded((int, int, int) dimensions)
+{
+    var sides = new [] { dimensions.Item1, dimensions.Item2, dimensions.Item3, }.OrderBy(i => i).ToArray();
+    var small1 = sides[0];
+    var small2 = sides[1];
+    return 2 * (small1 + small2) + sides.Aggregate(1, (curr, next) => curr *= next);
 }
 
 (int, int, int) ParseLine(string line)
